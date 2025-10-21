56f2a447a0564d5e3e79b14eb78459b76a5991f9
aldys002 <igidiaaldys@gmail.com>
2025-10-21 10:05:14 +0700

    menambahkan rute utama dan menjalankan server Express

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ${PORT}');
+});
