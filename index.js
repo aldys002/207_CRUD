445d5fcf2ba3f0ad254b9ee327ceee7e855d9099
aldys002 <igidiaaldys@gmail.com>
2025-10-21 10:37:59 +0700

    tambahkan endpoint DELETE /api/mahasiswa/:id untuk menghapus data mahasiswa berdasarkan ID

--- index.js
+++ index.js
@@ -77,4 +77,18 @@ app.put('/api/mahasiswa/:id', (req, res) => {
     );
 });
 
+app.delete('/api/mahasiswa/:id', (req, res) => {
+    const userId = req.params.id;
+
+    db.query('DELETE FROM mahasiswa WHERE id = ?', [userId], (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Database error' });
+        }
+
+        res.json({ message: 'User deleted successfully' });
+    });
+});
+
+
 
