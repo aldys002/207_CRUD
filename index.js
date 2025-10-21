56f6b1400c4fd8fbf95650aefab74fff47672aa9
aldys002 <igidiaaldys@gmail.com>
2025-10-21 10:33:06 +0700

    tambahkan endpoint PUT /api/mahasiswa/:id untuk update data mahasiswa berdasarkan ID

--- index.js
+++ index.js
@@ -59,5 +59,22 @@ app.post('/api/mahasiswa', (req, res) => {
         }
     );
 });
+app.put('/api/mahasiswa/:id', (req, res) => {
+    const userId = req.params.id;
+    const { nama, nim, kelas, prodi } = req.body;
+
+    db.query(
+        'UPDATE mahasiswa SET nama = ?, nim = ?, kelas = ?, prodi = ? WHERE id = ?',
+        [nama, nim, kelas, prodi, userId],
+        (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Database error' });
+            }
+
+            res.json({ message: 'User updated successfully' });
+        }
+    );
+});
 
 
